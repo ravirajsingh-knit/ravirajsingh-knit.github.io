92f4098a00d38abfdba158bc7046fa8c97a44b02
ravirajsingh-knit <ravirajsingh.knit@gmail.com>
2018-09-12 12:29:05 +0530

    Category

--- product/product.component.js
+++ product/product.component.js
@@ -5,5 +5,4 @@ angular.module("product").component("productDisplay",{
             
             
         }]
-    
 });
---
+++ product/product.filter.js
@@ -0,0 +1,14 @@
+angular.module("product").filter("productCat",function() {
+
+    return function(product, selector) {
+        if(!selector)
+            return product;
+        if(!selector[0]&&!selector[1]&&!selector[2])
+            return product;
+        
+
+        return product.filter(function(element, index, array) {
+            return (element.category=="Wear"&& selector[2])||(element.category=="Books"&& selector[1])||(element.category=="Electronic"&& selector[0]);
+            });    
+    };
+  });
